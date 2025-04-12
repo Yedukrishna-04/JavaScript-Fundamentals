83b92d3bfbbe86db46917be5b4e05d3b8078fac9
Yedukrishna-04 <nairyedukrishna2004@gmail.com>
2025-04-13 02:11:44 +0530

    Refactor forLoop.js: Comment out "Hello World!" example and add squared numbers calculation

--- Conditional and Control Statements/forLoop.js
+++ Conditional and Control Statements/forLoop.js
@@ -1,5 +1,12 @@
-var messsage = "Hello World!";
+// var messsage = "Hello World!";
 
-for (var i =1; i < 11; i++){
-    console.log(messsage+" " + i);
+// for (var i =1; i < 11; i++){
+//     console.log(messsage+" " + i);
+// }
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var squaredNumbers = [];
+for(var i = 0; i < numbers.length; i++){
+    squaredNumbers[i] = numbers[i] * numbers[i];
 }
+console.log(squaredNumbers);
