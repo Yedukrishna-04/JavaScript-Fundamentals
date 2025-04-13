9756cd8cdc7d93a17cc985eda799397b6d1e64ed
Yedukrishna-04 <115946007+Yedukrishna-04@users.noreply.github.com>
2025-04-13 10:33:18 +0530

    Update forLoop.js

--- Conditional and Control Statements/forLoop.js
+++ Conditional and Control Statements/forLoop.js
@@ -1,8 +1,8 @@
- var messsage = "Hello World!";
+ // var messsage = "Hello World!";
 
- for (var i =1; i < 11; i++){
-     console.log(messsage+" " + i);
- }
+ // for (var i =1; i < 11; i++){
+ //     console.log(messsage+" " + i);
+ // }
 
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var squaredNumbers = [];
