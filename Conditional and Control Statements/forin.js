947bea5991e9992426e7b61361b4cfe35b1ae523
Yedukrishna-04 <nairyedukrishna2004@gmail.com>
2025-04-13 10:45:00 +0530

    Remove commented-out code for colors object in forin.js

--- Conditional and Control Statements/forin.js
+++ Conditional and Control Statements/forin.js
@@ -1,9 +1,14 @@
-var colors = {
-    primary: "Blue",
-    secondary : "Red",
-    tertiary: "Green"
-}
+// var colors = {
+//     primary: "Blue",
+//     secondary : "Red",
+//     tertiary: "Green"
+// }
 
-for (var color in colors) {
-    console.log(color + "-->"+ colors[color]);
+// for (var color in colors) {
+//     console.log(color + "-->"+ colors[color]);
+// }
+
+var colorArray = ['Red', 'Green', 'Blue', 'Yellow', 'Orange'];
+for(var color in colorArray){
+    console.log(color + "-->" + colorArray[color]);
 }
