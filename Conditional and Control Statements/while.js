d56ca86c5283e1ca3445b81b4f6936f04019a807
Yedukrishna-04 <nairyedukrishna2004@gmail.com>
2025-04-13 10:31:47 +0530

    Add while.js: Implement do-while loop to display "Hello, World!" with starting value 5

---
+++ Conditional and Control Statements/while.js
@@ -0,0 +1,13 @@
+// var i = 1;
+// var n = 10;
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+var i = 5;
+var n = 10;
+do{
+    console.log("Hello, World"+" "+i);
+    i++;
+}while(i<=n)
