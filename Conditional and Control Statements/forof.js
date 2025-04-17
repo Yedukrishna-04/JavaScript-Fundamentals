91dbccb5251d5e9eab8e865e1fafc4d950d020c2
Yedukrishna-04 <nairyedukrishna2004@gmail.com>
2025-04-17 20:33:50 +0530

    Add forof.js: Implement for-of loop to iterate over arrays and strings

---
+++ Conditional and Control Statements/forof.js
@@ -0,0 +1,18 @@
+// The for...of loop is used to iterate over iterable objects such as arrays, strings, maps, sets, and more. It returns the values of the iterable object.
+
+var scores = [60, 80, 90, 75];
+for(var score of scores){
+    console.log(score);
+}
+console.log("--------------------------------------");
+
+var colors = ["Red", "Green", "Bluee", "Yellow", "Orange"];
+for(var[index, color] of colors.entries()){
+    console.log(index + "-->" + color);
+}
+console.log("--------------------------------------");
+var string = "Hello World!";
+for(var char of string){
+    console.log(char);
+}
+
